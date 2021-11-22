c9067f7a370ca36d071c3e3fb9b823d8add57301
Siaoyeu <68472472+vvvu@users.noreply.github.com>
2021-11-22 22:03:11 +0800

    Add files via upload

---
+++ QuanX_node_information.js
@@ -0,0 +1,12 @@
+if ($response.statusCode != 200) {
+  $done(Null);
+}
+
+var body = $response.body;
+var obj = JSON.parse(body);
+var title = obj['country'];
+var subtitle = obj['city'] + ' ' + obj['isp'];
+var ip = obj['query'];
+
+
+$done({title, subtitle, ip});
